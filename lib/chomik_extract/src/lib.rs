830e1d37b724a54acff5ae42c9f292e608cd2567
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-06-19 23:21:41 -0700

    feat(chomik_extract)!: return different `AnimFile` that includes filename
    - Created a new `chomik_extract::AnimFile` type (yes, a third `AnimFile`
      --- I should make better names) that also holds the filename of the
      `.anim` file. There's still `AnimFile::files`, with the same API and
      behavior, but it's a new type so breaking change it is.

--- lib/chomik_extract/src/lib.rs
+++ lib/chomik_extract/src/lib.rs
@@ -2,12 +2,33 @@ use std::io::{Read, Result, Seek};
 
 mod msi;
 
-pub fn extract_anims<R: Seek + Read>(msi: R) -> Result<Box<[qrc_parse::AnimFile]>> {
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct AnimFile {
+    filename: Box<str>,
+    parsed: qrc_parse::AnimFile,
+}
+
+impl AnimFile {
+    #[must_use]
+    pub fn filename(&self) -> &str {
+        &self.filename
+    }
+
+    #[must_use]
+    pub fn files(&self) -> Box<[qrc_parse::File<'_>]> {
+        self.parsed.files()
+    }
+}
+
+pub fn extract_anims<R: Seek + Read>(msi: R) -> Result<Box<[AnimFile]>> {
     let (anim_files, mut cabinets) = msi::extract_anims(msi)?;
 
     let mut parsed = Vec::new();
     for file in anim_files {
-        parsed.push(qrc_parse::AnimFile::parse(cabinets.get_file(&file)?.into_reader())?);
+        parsed.push(AnimFile {
+            filename: file.filename().into(),
+            parsed: qrc_parse::AnimFile::parse(cabinets.get_file(&file)?.into_reader())?,
+        });
     }
 
     Ok(parsed.into_boxed_slice())

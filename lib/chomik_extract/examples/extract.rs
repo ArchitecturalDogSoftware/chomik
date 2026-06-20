58765e1384a4452c85c632761578b3f9f56b4002
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-06-19 23:28:40 -0700

    docs(chomik_extract): added `extract` example program
    - Very much similar to `qrc_parse`'s `extract` example program.

---
+++ lib/chomik_extract/examples/extract.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let mut args = std::env::args();
+    let Some(path) = args.nth(1) else {
+        eprintln!("Usage: extract <path-to-msi-file>");
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "missing argument"));
+    };
+    let file = File::open(&path)?;
+
+    let extracted = chomik_extract::extract_anims(BufReader::new(file))?;
+    for anim in extracted {
+        println!("{}", anim.filename());
+        for file in anim.files() {
+            println!("    {} ({} bytes)", file.path(), file.data().len());
+        }
+    }
+
+    Ok(())
+}

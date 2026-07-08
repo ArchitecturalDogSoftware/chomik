9106b2195c8090aa9caa45b73f7ce4cba8872af3
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-07-08 16:19:02 -0700

    feat(chomik): file drag-and-drop animations
    - Added animations that trigger when a file is dragged over and then
      either dragged off or dropped onto the application.
      - Made refactors and bug fixes necessary to support this.
      - Not perfectly implemented as of yet, as it does not wait for the
        current animation to finish first.

--- src/animations.rs
+++ src/animations.rs
@@ -26,14 +26,14 @@ fn startup(
     ))
     .unwrap();
 
-    let idle_animations = IdleAnimations::from_set(&animations, asset_server.as_ref()).unwrap();
+    let animation_set = AnimationSet::from_set(&animations, asset_server.as_ref());
 
     commands.spawn((
-        Sprite::from_image(idle_animations.starting_image()),
-        idle_animations,
-        State::default(),
+        Sprite::from_image(animation_set.idle.first_image()),
+        State::default_with_timer(&animation_set.idle),
         AnimationTimer::from_fps(20.0),
     ));
+    commands.insert_resource(animation_set);
 
     for mut window in windows {
         window.visible = true;
@@ -43,35 +43,54 @@ fn startup(
 #[expect(clippy::needless_pass_by_value, reason = "`Res` is used for querying")]
 fn animate_sprite(
     time: Res<Time>,
-    idle_animations: Res<IdleAnimations>,
+    animation_set: Res<AnimationSet>,
+    mut file_dnd_reader: MessageReader<FileDragAndDrop>,
     mut query: Query<(&mut State, &mut AnimationTimer, &mut Sprite)>,
 ) {
+    let dnd_msg = file_dnd_reader.read().last();
+
     for (mut state, mut animation_timer, mut sprite) in &mut query {
         animation_timer.tick(time.delta());
-        state.tick(&idle_animations, time.delta(), &animation_timer, &mut sprite);
+        state.tick(&animation_set, time.delta(), dnd_msg, &animation_timer, &mut sprite);
     }
 }
 
 #[derive(Component)]
 enum State {
     Idle(IdleState),
+    FileDnd(FileDndState),
 }
 
 impl State {
+    fn default_with_timer(idle_animations: &IdleAnimations) -> Self {
+        Self::Idle(IdleState::default_with_timer(idle_animations))
+    }
+
     fn tick(
         &mut self,
-        idle_animations: &IdleAnimations,
+        animation_set: &AnimationSet,
         delta: Duration,
+        file_dnd_event: Option<&FileDragAndDrop>,
         // Expected to have already been ticked.
         animation_timer: &AnimationTimer,
         sprite: &mut Sprite,
     ) {
+        if let Some(file_dnd_event) = file_dnd_event
+            && !matches!(self, Self::FileDnd(_))
+        {
+            *self = Self::FileDnd(FileDndState::from_event(&animation_set.file_dnd, file_dnd_event, sprite));
+            return;
+        }
+
         let should_return_to_idle = match self {
-            Self::Idle(idle_state) => idle_state.tick(idle_animations, delta, animation_timer, sprite),
+            Self::Idle(idle_state) => idle_state.tick(&animation_set.idle, delta, animation_timer, sprite),
+            Self::FileDnd(file_dnd_state) => {
+                file_dnd_state.tick(&animation_set.file_dnd, file_dnd_event, delta, animation_timer, sprite)
+            }
         };
 
         if should_return_to_idle {
-            *self = Self::Idle(IdleState::default());
+            *self = Self::Idle(IdleState::default_with_timer(&animation_set.idle));
         }
     }
 }
@@ -88,6 +107,10 @@ enum IdleState {
 }
 
 impl IdleState {
+    fn default_with_timer(idle_animations: &IdleAnimations) -> Self {
+        Self::Main { timer: Timer::new(idle_animations.main_duration, TimerMode::Once), frame_idx: 0 }
+    }
+
     fn tick(
         &mut self,
         idle_animations: &IdleAnimations,
@@ -111,7 +134,7 @@ impl IdleState {
                             *frame_idx = 0;
                         }
 
-                        sprite.image = animation.frames[*frame_idx].clone();
+                        sprite.image = animation.get(*frame_idx);
                     }
 
                     false
@@ -125,7 +148,7 @@ impl IdleState {
                     if *frame_idx == animation.frames.len() {
                         true
                     } else {
-                        sprite.image = animation.frames[*frame_idx].clone();
+                        sprite.image = animation.get(*frame_idx);
                         false
                     }
                 } else {
@@ -164,8 +187,133 @@ impl Default for IdleState {
     }
 }
 
+enum FileDndState {
+    HoverStart { frame_idx: usize },
+    HoverLoop { frame_idx: usize },
+    HoverCancel { frame_idx: usize },
+    Drop { frame_idx: usize },
+}
+
+impl FileDndState {
+    fn from_event(
+        file_dnd_animations: &FileDndAnimations,
+        file_dnd_event: &FileDragAndDrop,
+        sprite: &mut Sprite,
+    ) -> Self {
+        // TO-DO: this doesn't actually filter for the window being dropped upon. This would be problematic if we
+        // spawned multiple windows. So not in scope now, but it would be hilarious to release a swarm of hamsters, so
+        // it's worth considering in the future.
+        match file_dnd_event {
+            FileDragAndDrop::DroppedFile { window: _, path_buf } => {
+                tracing::trace!("file '{}' dropped onto a sprite", path_buf.to_string_lossy());
+
+                sprite.image = file_dnd_animations.drop.first_image();
+                Self::Drop { frame_idx: 0 }
+            }
+            FileDragAndDrop::HoveredFile { window: _, path_buf } => {
+                tracing::trace!("file '{}' hovered over a sprite", path_buf.to_string_lossy());
+
+                sprite.image = file_dnd_animations.hover_start.first_image();
+                Self::HoverStart { frame_idx: 0 }
+            }
+            FileDragAndDrop::HoveredFileCanceled { window: _ } => {
+                tracing::trace!("file hover over sprite canceled");
+
+                sprite.image = file_dnd_animations.hover_cancel.first_image();
+                Self::HoverCancel { frame_idx: 0 }
+            }
+        }
+    }
+
+    fn tick(
+        &mut self,
+        file_dnd_animations: &FileDndAnimations,
+        file_dnd_event: Option<&FileDragAndDrop>,
+        delta: Duration,
+        // Expected to have already been ticked.
+        animation_timer: &AnimationTimer,
+        sprite: &mut Sprite,
+    ) -> bool {
+        // TO-DO: wait for existing animation to finish.
+        if let Some(event) = file_dnd_event {
+            *self = Self::from_event(file_dnd_animations, event, sprite);
+            return false;
+        }
+
+        if !animation_timer.just_finished() {
+            return false;
+        }
+
+        match self {
+            Self::HoverStart { frame_idx } => {
+                let animation = &file_dnd_animations.hover_start;
+
+                *frame_idx += 1;
+                if *frame_idx == animation.frames.len() {
+                    sprite.image = file_dnd_animations.hover_loop.first_image();
+                    *self = Self::HoverLoop { frame_idx: 0 };
+                } else {
+                    sprite.image = animation.get(*frame_idx);
+                }
+
+                false
+            }
+            Self::HoverLoop { frame_idx } => {
+                let animation = &file_dnd_animations.hover_loop;
+
+                *frame_idx += 1;
+                if *frame_idx == animation.frames.len() {
+                    *frame_idx = 0;
+                }
+
+                sprite.image = animation.get(*frame_idx);
+
+                false
+            }
+            Self::HoverCancel { frame_idx } => {
+                let animation = &file_dnd_animations.hover_cancel;
+
+                *frame_idx += 1;
+                if *frame_idx == animation.frames.len() {
+                    return true;
+                }
+
+                sprite.image = animation.get(*frame_idx);
+
+                false
+            }
+            Self::Drop { frame_idx } => {
+                let animation = &file_dnd_animations.drop;
+
+                *frame_idx += 1;
+                if *frame_idx == animation.frames.len() {
+                    return true;
+                }
+
+                sprite.image = animation.get(*frame_idx);
+
+                false
+            }
+        }
+    }
+}
+
 #[derive(Resource)]
 #[component(immutable)]
+struct AnimationSet {
+    idle: IdleAnimations,
+    file_dnd: FileDndAnimations,
+}
+
+impl AnimationSet {
+    fn from_set(set: &chomik_extract::AnimationSet, asset_server: &AssetServer) -> Self {
+        Self {
+            idle: IdleAnimations::from_set(set, asset_server).unwrap(),
+            file_dnd: FileDndAnimations::from_set(set, asset_server).unwrap(),
+        }
+    }
+}
+
 struct IdleAnimations {
     main: Animation,
     main_duration: Duration,
@@ -234,8 +382,38 @@ impl IdleAnimations {
         rand::rng().sample(&self.weighted_distribution)
     }
 
-    fn starting_image(&self) -> Handle<Image> {
-        self.main.frames[0].clone()
+    fn first_image(&self) -> Handle<Image> {
+        self.main.first_image()
+    }
+}
+
+struct FileDndAnimations {
+    hover_start: Animation,
+    hover_loop: Animation,
+    hover_cancel: Animation,
+    drop: Animation,
+}
+
+impl FileDndAnimations {
+    fn from_set(
+        chomik_extract::AnimationSet { file_over, file_drop, .. }: &chomik_extract::AnimationSet,
+        asset_server: &AssetServer,
+    ) -> Result<Self, image::ImageError> {
+        let chomik_extract::LoopingAnimation { name: _, entrance, looping, exit } = match file_over {
+            chomik_extract::Animation::Looping(file_over) => file_over,
+            _ => panic!("Received one-shot file over animation, expected looping animation"),
+        };
+        let chomik_extract::OneShotAnimation { name: _, sequence: file_drop } = match file_drop {
+            chomik_extract::Animation::OneShot(file_drop) => file_drop,
+            _ => panic!("Received looping file drop animation, expected one-shot animation"),
+        };
+
+        let hover_start = Animation::from_sequence(entrance, asset_server)?;
+        let hover_loop = Animation::from_sequence(looping, asset_server)?;
+        let hover_cancel = Animation::from_sequence(exit, asset_server)?;
+        let drop = Animation::from_sequence(file_drop, asset_server)?;
+
+        Ok(Self { hover_start, hover_loop, hover_cancel, drop })
     }
 }
 
@@ -303,5 +481,13 @@ impl Animation {
             .collect::<image::ImageResult<_>>()?;
 
         Ok(Self { frames })
+    }
+
+    fn get(&self, idx: usize) -> Handle<Image> {
+        self.frames[idx].clone()
+    }
+
+    fn first_image(&self) -> Handle<Image> {
+        self.get(0)
     }
 }
